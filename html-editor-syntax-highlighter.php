0f0a4eae30cd2ef3c8125e70b30bb772d2ed86ea
arniebradfo <arniebradfo@gmail.com>
2016-06-12 10:35:30 -0600

    added version string to js and css resources

--- html-editor-syntax-highlighter.php
+++ html-editor-syntax-highlighter.php
@@ -31,17 +31,17 @@ define('HESH_LIBS', plugins_url('/lib/',__FILE__));
 class wp_html_editor_syntax {
 
 	public function __construct () {
-		if (!$this->is_editor()) return;
+		if (!self::is_editor()) return;
 		add_action('admin_enqueue_scripts', array(&$this, 'admin_enqueue_scripts'));
 	}
 
 	// Enqueues scripts and styles for hesh.js
 	public function admin_enqueue_scripts () {
-		wp_enqueue_style('codemirror', HESH_LIBS.'codemirror.min.css');
-		wp_enqueue_style('heshcss', HESH_LIBS.'hesh.min.css');
-		wp_register_script('codemirror', HESH_LIBS.'codemirror.min.js', false, false, true);
+		wp_enqueue_style('codemirror', HESH_LIBS.'codemirror.min.css', false, self::version());
+		wp_enqueue_style('heshcss', HESH_LIBS.'hesh.min.css', false, self::version());
+		wp_register_script('codemirror', HESH_LIBS.'codemirror.min.js', false, self::version(), true);
 		wp_enqueue_script('codemirror');
-		wp_register_script('heshjs', HESH_LIBS.'hesh.min.js', array('codemirror'), false, true); // 'tiny_mce' dependency doesn't work?!
+		wp_register_script('heshjs', HESH_LIBS.'hesh.min.js', array('codemirror'), self::version(), true); // 'tiny_mce' dependency doesn't work?!
 		wp_enqueue_script('heshjs');
 	}
 
@@ -51,6 +51,12 @@ class wp_html_editor_syntax {
 			!strstr($_SERVER['SCRIPT_NAME'], 'post-new.php'))
 			return false;
 		return true;
+	}
+
+	// returns the current version of the plugin
+	// this will be appended to the end of the js and css files for cache-busting purposes
+	private function version () {
+		return get_plugin_data( __FILE__ )['Version'];
 	}
 
 }
