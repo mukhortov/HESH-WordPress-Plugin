4ff623398ddab4fa2f79cc84064984a253361765
arniebradfo <arniebradfo@gmail.com>
2017-01-26 00:25:41 -0700

    fixed wp buttoncontent addition problem

--- dist/hesh.js
+++ dist/hesh.js
@@ -11,7 +11,6 @@
 	document,
 	window,
 	CodeMirror,
-	// CodeMirrorCSS,
 	wpLink,
 	switchEditors
 ) {
@@ -73,117 +72,40 @@
 
 		var runEditor = function () {
 			editor = CodeMirror.fromTextArea(target, options);
-			// addButtons();
 
-			// // Save changes to the textarea on the fly
-			// editor.on('change', function () {
-			// 	editor.save();
-			// 	clearTimeout(ontypeSaveTimer);
-			// 	ontypeSaveTimer = setTimeout(updateTextareaHeight, 3000);
+			// Save changes to the textarea on the fly
+			// var ontypeSaveTimer;
+			editor.on('change', function () {
+				// console.log(editor);
+				// editor.getTextArea().value
+
+				// editor.doc.setValue(editor.getTextArea().value);
+				console.log(editor.doc.getValue().length);
+				console.log(editor.getTextArea().value.length);
+				editor.save();
+				console.log(editor.getTextArea().value.length);
+				// clearTimeout(ontypeSaveTimer);
+				// ontypeSaveTimer = setTimeout(updateTextareaHeight, 3000);
+			});
+
+			window.setInterval(function() {
+				if (editor.doc.getValue().length !== editor.getTextArea().value.length) {
+					editor.doc.setValue(editor.getTextArea().value);
+					console.log('mismatch');
+				}
+			}, 50);
+
+			// console.log(editor.getTextArea());
+			// editor.getTextArea().addEventListener('change', function (e) {
+			// 	console.log(e);
+			// 	console.log('here');
 			// });
-
-			// // Saving cursor state
-			// editor.on('cursorActivity', function () {
-			// 	var curPos = editor.getCursor();
-			// 	document.cookie = 'hesh_plugin_pos=' + postID + ',' + curPos.line + ',' + curPos.ch;
-			// });
-
-			// // Restoring cursor state
-			// var curPos = (getCookie('hesh_plugin_pos') || '0,0,0').split(',');
-			// if (postID === curPos[0]) {
-			// 	editor.setCursor(parseFloat(curPos[1]), parseFloat(curPos[2]));
-			// }
 
 			resizeEditor();
-			// remapAddMedia();
-			// changeFontSize();
 			editor.setOption('theme', 'material');
 			isOn = true;
-
-			// updateTabBarPaddings();
-			// window.addEventListener('resize', windowResized);
-
-			// Fix for floating tabbar in full-height mode
-			// window.setTimeout(updateTextareaHeight, 3000);
 		};
 
-		// var updateTabBarPaddings = function () {
-		// 	var CodeM = document.querySelector('.CodeMirror');
-		// 	CodeM.style.marginTop = toolbar.clientHeight + 'px';
-		// };
-
-		// var windowResizeTimer;
-		// var windowResized = function () {
-		// 	clearTimeout(windowResizeTimer);
-		// 	windowResizeTimer = setTimeout(updateTabBarPaddings, 250);
-		// };
-
-		// var ontypeSaveTimer;
-		// var updateTextareaHeight = function () {
-		// 	if (document.querySelector('.CodeMirror') !== null) {
-		// 		document.querySelector('textarea.wp-editor-area').style.height = document.querySelector('.CodeMirror').clientHeight + 'px';
-		// 	}
-		// };
-
-		// var addButtons = function () {
-		// 	if (!buttonsAdded) {
-		// 		var toolbarVars = {
-		// 			more: ['<!--more-->', ''],
-		// 			comment: ['<!-- ', ' -->'],
-		// 			code: ['<code>', '</code>'],
-		// 			li: ['<li>', '</li>'],
-		// 			ol: ['<ol>', '</ol>'],
-		// 			ul: ['<ul>', '</ul>'],
-		// 			img: ['<img src="$" alt="', '">', 'Enter the URL of the image'],
-		// 			ins: ['<ins>', '</ins>'],
-		// 			del: ['<del>', '</del>'],
-		// 			link: ['<a href="$">', '</a>', 'Enter the destination URL'],
-		// 			blockquote:	['\r<blockquote>', '</blockquote>\r'],
-		// 			h3: ['<h3>', '</h3>'],
-		// 			h2: ['<h2>', '</h2>'],
-		// 			h1: ['<h1>', '</h1>'],
-		// 			p: ['<p>', '</p>'],
-		// 			i: ['<em>', '</em>'],
-		// 			b: ['<strong>', '</strong>']
-		// 		};
-		// 		for (var key in toolbarVars) {
-		// 			var t = toolbarVars[key];
-		// 			var t3 = t[2] ? 'data-prompt="' + t[2] + '"' : '';
-		// 			toolbar.insertAdjacentHTML('afterbegin', '<input type="button" id="cm_content_' + key + '" data-start=\'' + t[0] + '\' data-end=\'' + t[1] + '\' ' + t3 + ' class="ed_button button cm_ed_button" value="' + key + '">');
-		// 			document.getElementById('cm_content_' + key).onclick = buttonClick;
-		// 		}
-		// 		// themeSwitcher();
-		// 		// fontSizeSwitcher();
-		// 		// fullscreen();
-		// 		buttonsAdded = true;
-		// 	}
-		// };
-
-		// var buttonClick = function () {
-		// 	var selStart = editor.getCursor('start');
-		// 	var start = this.getAttribute('data-start');
-		// 	var end = this.getAttribute('data-end');
-		// 	var cmPrompt = this.getAttribute('data-prompt') || null;
-		// 	var selText = editor.getSelection();
-		// 	if (this.id === 'cm_content_link' && wpLink) { // Native WP link popup window
-		// 		wpLink.open();
-		// 		document.getElementById('wp-link-submit').onclick = function () {
-		// 			var attrs = wpLink.getAttrs();
-		// 			start = '<a href="' + attrs.href + '" title="' + attrs.title + '" target="' + attrs.target + '">';
-		// 			editor.replaceSelection(start + selText + '</a>');
-		// 			wpLink.close();
-		// 			// editor.setSelection(selStart, editor.getCursor('end'));
-		// 			editor.setCursor(selStart.line, selStart.ch + start.length);
-		// 			editor.focus();
-		// 		};
-		// 	} else {
-		// 		if (cmPrompt) start = start.replace('$', window.prompt(cmPrompt, ''));
-		// 		editor.replaceSelection(start + selText + end);
-		// 		// editor.setSelection(selStart, editor.getCursor('end'));
-		// 		editor.setCursor(selStart.line, selStart.ch + start.length);
-		// 		editor.focus();
-		// 	}
-		// };
 
 		var toVisual = function () {
 			if (isOn) {
@@ -191,7 +113,6 @@
 				editor.toTextArea();
 				tab_html.onclick = toHTML;
 				isOn = false;
-				// window.removeEventListener('resize', windowResized);
 			}
 		};
 
@@ -206,8 +127,6 @@
 		var resizeEditor = function () {
 			var target = document.querySelector('.CodeMirror');
 			var handle = document.getElementById('content-resize-handle');
-			// var handle = document.createElement('div');
-			// var hwrap = document.getElementById('wp-content-wrap');
 			var offsetTop = target.getBoundingClientRect().top;
 			var move = function (e) {
 				// TODO: fix this
@@ -216,8 +135,6 @@
 				target.style.height = height + 'px';
 				window.getSelection().removeAllRanges(); // disable selection on resize
 			};
-			// handle.className = 'content-resize-handle';
-			// hwrap.appendChild(handle);
 			handle.addEventListener('mousedown', function () {
 				document.addEventListener('mousemove', move);
 			});
@@ -225,19 +142,6 @@
 				document.removeEventListener('mousemove', move);
 			});
 		};
-
-		// var remapAddMedia = function () {
-		// 	window.send_to_editor_wp = window.send_to_editor; // remap wp native func
-		// 	var send_to_editor = function (html) {
-		// 		if (isOn && window.wpActiveEditor === 'content') {
-		// 			editor.replaceSelection(html);
-		// 			editor.save();
-		// 		} else {
-		// 			window.send_to_editor_wp(html);
-		// 		}
-		// 	};
-		// 	window.send_to_editor = send_to_editor;
-		// };
 
 		// Initialise //
 		if (visualEditorEnabled && visualEditorActive) {
@@ -262,7 +166,6 @@
 	document, 
 	window, 
 	window.CodeMirror, 
-	// CodeMirrorCSS, 
-	window.wpLink, 
+	window.wpLink,
 	window.switchEditors
 );
