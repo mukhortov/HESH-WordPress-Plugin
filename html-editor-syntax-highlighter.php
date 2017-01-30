2edf755066778c9f50f64d10cd59378bf5078977
arniebradfo <arniebradfo@gmail.com>
2017-01-30 00:48:04 -0500

    added options panel

--- html-editor-syntax-highlighter.php
+++ html-editor-syntax-highlighter.php
@@ -1,7 +1,8 @@
 <?php
+
+
 /**
- *
- * @since              1.7.2
+* @since              1.7.2
  * @package            HESH_plugin
  *
  * Plugin Name:        HTML Editor Syntax Highlighter - DEV
@@ -29,24 +30,26 @@ if ( preg_match( '#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'] ) ) {
 define( 'HESH_LIBS', plugins_url( '/dist/', __FILE__ ) );
 
 class wp_html_editor_syntax {
-
+	
 	public function __construct () {
 		add_action( 'admin_enqueue_scripts', array(&$this, 'admin_enqueue_scripts' ) );
+		add_action( 'wp_ajax_hesh_options_form', 'hesh_options_process');
+		add_action( 'admin_footer', array(&$this, 'hesh_print_form') );
 	}
-
+	
 	// Enqueues scripts and styles for hesh.js
 	public function admin_enqueue_scripts () {
-
-		if (!strstr($_SERVER['SCRIPT_NAME'], 'post.php') &&
-			!strstr($_SERVER['SCRIPT_NAME'], 'post-new.php')) return;
-
-		$plugData = get_plugin_data( __FILE__ ); // need this temporary var to support versions of php < 5.4
+		
+		if (!strstr($_SERVER['SCRIPT_NAME'], 'post.php') && !strstr($_SERVER['SCRIPT_NAME'], 'post-new.php')) return;
+		
+		$plugData = get_plugin_data( __FILE__ );
+		// need this temporary var to support versions of php < 5.4
 		$ver = $plugData['Version'];
-
+		
 		wp_enqueue_style( 'codemirror', HESH_LIBS.'codemirror.css', false, $ver );
 		wp_enqueue_style( 'material', HESH_LIBS.'material.css', false, $ver );
 		wp_enqueue_style( 'heshcss', HESH_LIBS.'hesh.css', false, $ver );
-
+		
 		wp_enqueue_script( 'codemirror', HESH_LIBS.'codemirror.js', false, $ver, true );
 		wp_enqueue_script( 'xml_cm', HESH_LIBS.'xml.js', false, $ver, true );
 		wp_enqueue_script( 'javascript_cm', HESH_LIBS.'javascript.js', false, $ver, true );
@@ -55,26 +58,95 @@ class wp_html_editor_syntax {
 		wp_enqueue_script( 'shortcode_cm', HESH_LIBS.'shortcode.js', array('codemirror'), $ver, true );
 		wp_enqueue_script( 'wordpresspost_cm', HESH_LIBS.'wordpresspost.js', array('codemirror', 'htmlmixed_cm', 'shortcode_cm'), $ver, true );
 		wp_enqueue_script( 'heshjs', HESH_LIBS.'hesh.js', array('codemirror'), $ver, true );
+		
+		update_user_meta( get_current_user_id(), 'hesh_theme', 'material');
+		$metaTheme = get_user_meta( get_current_user_id(), 'hesh_theme' );
 
+		wp_localize_script( 
+			'heshjs', // i think... // the handle for the js // the_unique_name_for_your_js
+			'heshJS', // theUniqueNameForYourJSObject
+			array(
+				'ajaxUrl' => admin_url( 'admin-ajax.php' ), // url for php file that process ajax request to WP
+				'nonce' => wp_create_nonce( 'hesh_nonce_id' ), // this is a unique token to prevent form hijacking
+				'metavalues' => $metaTheme // I will add the user options here
+			)
+		);
+		
+	}
+	
+	// 	AJAX forms
+	// 	https://teamtreehouse.com/community/submitting-a-form-in-wordpress-using-ajax
+	// 	http://wordpress.stackexchange.com/questions/60758/how-to-handle-form-submission
+	
+	
+	public function hesh_options_form_process() {
+		// 	process user settings
+		// 	https://developer.wordpress.org/plugins/users/working-with-user-metadata/
+		// 	https://codex.wordpress.org/Function_Reference/update_user_meta
+		# add user meta
+	}
+	
+	private function output_select_element($OptsArray){
+		# code...
 	}
 
-	// AJAX forms
-	// https://teamtreehouse.com/community/submitting-a-form-in-wordpress-using-ajax
-	// http://wordpress.stackexchange.com/questions/60758/how-to-handle-form-submission
-
-	// user settings
-	// https://developer.wordpress.org/plugins/users/working-with-user-metadata/
-	// https://codex.wordpress.org/Function_Reference/update_user_meta
-
-
-	public function setup_options() {
+	private function output_input_element($OptsArray){
 		# code...
+	}
+	
+	public function hesh_print_form() {
+		// ob_start();
+		?>
+			<div class="CodeMirror-settings closed" id="CodeMirror-settings" style="display:none;">
+				<div class="CodeMirror-settings__wrapper">
+					<header class="CodeMirror-settings__header CodeMirror-settings__docked">
+						<h2 class="CodeMirror-settings__title">Code Editor Settings</h2>
+					</header>
+					<form
+						action="<?php echo admin_url('admin-ajax.php');?>" 
+						method="post" 
+						class="form CodeMirror-settings__form" 
+						>
+						<?php wp_nonce_field('hesh_nonce_id','security-code-here');?>
+						<input name="action" value="hesh_nonce_id" type="hidden">
+
+						<table class="form-table"><tbody>
+							<tr>
+								<th scope="row"><label for="blogdescription">Tagline</label></th>
+								<td><input name="blogdescription" type="text" id="blogdescription" aria-describedby="tagline-description" value="Just another WordPress site" class="regular-text">
+								<p class="description" id="tagline-description">In a few words, explain what this site is about.</p></td>
+							</tr>
+						</tbody></table>
+					</form>
+					<footer class="CodeMirror-settings__footer CodeMirror-settings__docked">
+						<p class="CodeMirror-settings__foot-content CodeMirror-settings__feedback">
+							<small>
+								Submit a 
+								<a href="#" target="_blank">bug report</a> 
+								or 
+								<a href="#" target="_blank">feature request</a>.
+							</small>
+						</p>
+						<p class="CodeMirror-settings__foot-content CodeMirror-settings__credits">
+							<small>
+								Created by 
+								<a href="#" target="_blank">James Bradford</a> 
+								&amp; 
+								<a href="#" target="_blank">Petr Mukhortov</a>.
+							</small>
+						</p>
+					</footer>
+				</div>
+				<div class="CodeMirror-settings__toggle">X</div>
+			</div>
+		<?php 
+		// return ob_get_clean();
 	}
 
 }
 
 if ( is_admin() ) {
-	$hesh = new wp_html_editor_syntax();
+$hesh = new wp_html_editor_syntax();
 }
 
 ?>
--- src/hesh.js
+++ src/hesh.js
@@ -7,135 +7,144 @@
  * @since    1.7.2
 */
 
+console.log(heshJS); // from wordpress php
+
 (function (
 	document,
 	window,
 	CodeMirror,
-	wpLink,
 	switchEditors
 ) {
 	'use strict';
 
-	function heshPlugin () {
-		var editor;
-		var isOn = false;
-		var buttonsAdded = false;
-		var target = document.getElementById('content');
-		var postID = document.getElementById('post_ID') != null ? document.getElementById('post_ID').value : 0;
-		var tab_html = document.getElementById('content-html');
-		var tab_tmce = document.getElementById('content-tmce');
-		var visualEditorActive = document.getElementById('wp-content-wrap').className.indexOf('tmce-active') > -1;
-		var visualEditorEnabled = document.getElementById('content-tmce') != null;
-		var toolbar = document.getElementById('ed_toolbar');
-		var fullscreenBox = document.getElementById('wp-content-editor-container');
-		var fullscreenClass = 'heshFullscreen';
-		var publishButton = document.getElementById('save-post') != null ? document.getElementById('save-post') : document.getElementById('publish');
+	var editor;
+	var settingsPanel = document.getElementById('CodeMirror-settings');
+	var isActive = false;
+	var target = document.getElementById('content');
+	var tab_html = document.getElementById('content-html');
+	var tab_tmce = document.getElementById('content-tmce');
+	var visualEditorActive = document.getElementById('wp-content-wrap').className.indexOf('tmce-active') > -1;
+	var visualEditorEnabled = document.getElementById('content-tmce') != null;
+	var publishButton =  document.getElementById('save-post') || document.getElementById('publish');
 
 
-		var options = {
-			mode: 'wordpresspost',
-			tabMode: 'indent',
-			theme: 'material',
-			lineNumbers: true,
-			matchBrackets: true,
-			indentUnit: 4,
-			indentWithTabs: true,
-			enterMode: 'keep',
-			lineWrapping: true,
-			autofocus: true,
-			styleActiveLine: true,
-			electricChars: false,
-			extraKeys: {
-				'F11': function () {
-					// toggleFullscreen();
-				},
-				'Esc': function () {
-					// toggleFullscreen();
-				},
-				'Ctrl-S': function () {
-					publishButton.click();
-				},
-				'Cmd-S': function () {
-					publishButton.click();
-				}
+	var options = {
+		mode: 'wordpresspost',
+		tabMode: 'indent',
+		theme: 'material',
+		lineNumbers: true,
+		matchBrackets: true,
+		indentUnit: 4,
+		indentWithTabs: true,
+		enterMode: 'keep',
+		lineWrapping: true,
+		autofocus: true,
+		styleActiveLine: true,
+		electricChars: false,
+		extraKeys: {
+			'F11': function () {
+				// toggleFullscreen();
+			},
+			'Esc': function () {
+				// toggleFullscreen();
+			},
+			'Ctrl-S': function () {
+				publishButton.click();
+			},
+			'Cmd-S': function () {
+				publishButton.click();
 			}
-		};
+		}
+	};
 
 
-		var matchTextAreaHeight = function () {
+	function attachResize() {
+		function matchTextAreaHeight() {
 			editor.getWrapperElement().style.height = editor.getTextArea().style.height
 		}
 
+		document.getElementById('content-resize-handle').addEventListener('mousedown', function () {
+			document.addEventListener('mousemove', matchTextAreaHeight);
+		});
+		document.addEventListener('mouseup', function () {
+			document.removeEventListener('mousemove', matchTextAreaHeight);
+		});
 
-		var runEditor = function () {
-			editor = CodeMirror.fromTextArea(target, options);
+		matchTextAreaHeight();
 
-			// Save changes to the textarea on the fly
-			editor.on('change', function () {
-				editor.save();
-			});
-
-			// Check if any edits were made to the textarea.value
-			window.setInterval(function() {
-				if (editor.doc.getValue().length !== editor.getTextArea().value.length) {
-					editor.doc.setValue(editor.getTextArea().value);
-				}
-			}, 50);
-
-			document.getElementById('content-resize-handle').addEventListener('mousedown', function () {
-				document.addEventListener('mousemove', matchTextAreaHeight);
-			});
-			document.addEventListener('mouseup', function () {
-				document.removeEventListener('mousemove', matchTextAreaHeight);
-			});
-
-			matchTextAreaHeight();
-			isOn = true;
-		};
+	}
 
 
-		var toVisual = function () {
-			if (isOn) {
-				if (switchEditors.switchto) switchEditors.switchto(this);
-				editor.toTextArea();
-				tab_html.onclick = toHTML;
-				isOn = false;
+	function attachSettings() {
+		editor.getWrapperElement().appendChild(settingsPanel);
+		settingsPanel.style.display = 'block';
+		settingsPanel.querySelector('.CodeMirror-settings__toggle').addEventListener('click', toggleSettings);
+	}
+	function toggleSettings(event) {
+			// TODO: review browser support for toggle
+			settingsPanel.classList.toggle('open');
+			settingsPanel.classList.toggle('closed');
+	}
+
+	function startEditor() {
+		editor = CodeMirror.fromTextArea(target, options);
+
+		// Save changes to the textarea on the fly
+		editor.on('change', function () {
+			editor.save();
+		});
+
+		// Check if any edits were made to the textarea.value
+		window.setInterval(function() {
+			if (editor.doc.getValue().length !== editor.getTextArea().value.length) {
+				editor.doc.setValue(editor.getTextArea().value);
 			}
-		};
+		}, 50);
+	}
 
+	function toVisual() {
+		if (isActive) {
+			if (switchEditors.switchto) switchEditors.switchto(this);
+			editor.toTextArea();
+			tab_html.onclick = toHTML;
+			isActive = false;
+		}
+	}
 
-		var toHTML = function () {
-			if (!isOn) {
-				if (switchEditors.switchto) switchEditors.switchto(this);
-				window.setTimeout(runEditor, 0);
-				tab_tmce.onclick = toVisual;
-			}
-		};
+	function toHTML() {
+		if (!isActive) {
+			if (switchEditors.switchto) switchEditors.switchto(this);
+			window.setTimeout(runEditor, 0);
+			tab_tmce.onclick = toVisual;
+		}
+	}
 
+	function runEditor() {
+		startEditor();
+		attachResize();
+		attachSettings();
+		isActive = true;
+	};
 
-		// Initialise //
+	function initialise() {
 		if (visualEditorEnabled && visualEditorActive) {
 			tab_html.onclick = toHTML;
 		} else {
 			runEditor();
-			if (visualEditorEnabled) {
-				tab_tmce.onclick = toVisual;
-			} else {
-				document.body.className += ' visual-editor-is-disabled';
-			}
+			if (visualEditorEnabled) tab_tmce.onclick = toVisual;
+			else document.body.className += ' visual-editor-is-disabled';
 		}
 	}
 
 	if (document.readyState !== 'complete') {
-		if (window.addEventListener) window.addEventListener('load', heshPlugin, false);
-		else if (window.attachEvent) window.attachEvent('onload', heshPlugin);
+		if (window.addEventListener) window.addEventListener('load', initialise, false);
+		else if (window.attachEvent) window.attachEvent('onload', initialise);
 	} else {
-		heshPlugin();
+		initialise();
 	}
 })(
 	document, 
 	window, 
 	window.CodeMirror, 
-	window.wpLink,
 	window.switchEditors
 );
